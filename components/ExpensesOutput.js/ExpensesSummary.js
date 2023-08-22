f227805b59013940c1d0014aa47d9e4c48082cb3
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-21 17:45:57 -0700

    outline for expenses output

---
+++ components/ExpensesOutput.js/ExpensesSummary.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const ExpensesSummary = () => {
+  return (
+    <View>
+      <Text> Last 7 Days</Text>
+      <Text> Sum in Dollars </Text>
+      Summary List of expenses
+    </View>
+  );
+};
+
+export default ExpensesSummary;

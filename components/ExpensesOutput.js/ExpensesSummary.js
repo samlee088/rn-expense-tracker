a6480d6b454c40b0cb883b1eb766777204f70c9e
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-21 17:52:13 -0700

    expensesSummary component

--- components/ExpensesOutput.js/ExpensesSummary.js
+++ components/ExpensesOutput.js/ExpensesSummary.js
@@ -1,11 +1,14 @@
 import { Text, View } from "react-native";
 
-const ExpensesSummary = () => {
+const ExpensesSummary = ({ expenses, periodName }) => {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+
   return (
     <View>
-      <Text> Last 7 Days</Text>
-      <Text> Sum in Dollars </Text>
-      Summary List of expenses
+      <Text> {periodName} </Text>
+      <Text> ${expensesSum.toFixed(2)} </Text>
     </View>
   );
 };

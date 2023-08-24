073f46c870fe2254380af28959e69b4e035a8f5f
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-23 19:33:47 -0700

    update with modal, manageexpense page

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native"
+import { useLayoutEffect } from "react";
+import { Text, View } from "react-native";
 
+const ManageExpense = ({ route, navigation }) => {
+  const editedExpenseId = route.params?.expenseId;
 
-const ManageExpense = () => {
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edited Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
-      <View>
-        <Text>ManageExpense  </Text>
-      </View>
-  )
-}
+    <View>
+      <Text>ManageExpense </Text>
+    </View>
+  );
+};
 
-export default ManageExpense
+export default ManageExpense;
